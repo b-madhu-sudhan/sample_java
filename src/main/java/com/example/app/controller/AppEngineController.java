ae5fe4f6e63c193fd31cf5d02b2a682171b2c7c2
madhusudhan <madhu.b@thoughtgreen.com>
2022-06-24 14:33:03 +0530

    Java application as sample 1

---
+++ src/main/java/com/example/app/controller/AppEngineController.java
@@ -0,0 +1,15 @@
+package com.example.app.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppEngineController {
+
+    @GetMapping("/home")
+    public String index() {
+        return "Hello Computer";
+    }
+
+}
